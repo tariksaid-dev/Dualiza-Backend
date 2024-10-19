2de818b41de59da87daf6af93aba53ac2872bde8
tariksaid-dev <tariksaid.dev@gmail.com>
2024-10-19 06:33:27 +0200

    Add common module and pagination dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NewsModule } from './news/news.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { NewsModule } from './news/news.module';
       synchronize: true,
     }),
     NewsModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
